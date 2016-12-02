4e013ce39f5c8a0fed00f36c1712111f760142ec
Kamil Zień <kamilxzien@gmail.com>
2016-12-02 18:41:59 +0100

    obiekty dla kombinatoryki

---
+++ Advent/SQLFiddle.sql
@@ -0,0 +1,12 @@
+﻿USE AdventureWorks2012
+GO
+
+
+SELECT
+	a.productid
+	,b.productid
+FROM productdeployplan a
+CROSS APPLY (SELECT
+		b.productid
+	FROM productdeployplan b
+	WHERE b.defaultposition > a.defaultposition) b
---
+++ Advent/Security/Tests.sql
@@ -0,0 +1,8 @@
+﻿CREATE SCHEMA [Tests]
+GO
+
+
+EXECUTE sp_addextendedproperty	@name = N'MS_Description'
+								,@value = N'Contains solutions to a interesting problems.'
+								,@level0type = N'SCHEMA'
+								,@level0name = N'Tests';
